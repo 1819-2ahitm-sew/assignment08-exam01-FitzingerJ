615ad212e382e4e0922cfadbdff5c07590497393
jFitzinger <jacki.fitzinger@gmail.com>
2018-11-16 10:09:34 +0100

    first commit

--- src/main/java/at/htl/exam01/compress/StringCompress.java
+++ src/main/java/at/htl/exam01/compress/StringCompress.java
@@ -41,9 +41,40 @@ public class StringCompress {
      * @return String-Array mit dem entpacktem Text
      */
     public String[] readFromFile(String fileName) {
+        StringBuilder sb = new StringBuilder();
+        String[] textArray = new String[3];
+        int lines = getNoOfLines(fileName);
+        int count = 0;
+        int number = 0;
+        try (Scanner scanner = new Scanner(new FileReader(fileName))) {
+            do {
 
+                String text = scanner.nextLine();
+                for (int i = 1; i < text.length(); i++) {
+                    number = (int)text.charAt(i) - 48;
+                }
+                number++;
+                for (int i = 1; i < number; i++) {
+                    String help = "";
+                    help += text.charAt(0);
+                    textArray[count] = help;
+                    textArray[count] += help;
+                }
+                textArray[count + 1] = "\n";
+            /*for (int i = 1; i < text.length(); i++) {
+                count = (int)text.charAt(i) - 48;
+                scanner.nextLine();
+            }
+            for (int i = 0; i < count; i++) {
+                textArray[i] = text.charAt(0);
+            }*/
+            count += 2;
+            } while (count < lines * 2);
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
 
-        return null;
+        return textArray;
     }
 
 
@@ -54,7 +85,9 @@ public class StringCompress {
      * @param lines String-Array
      */
     public void print(String[] lines) {
-
+        for (int i = 0; i < lines.length; i++) {
+            System.out.print(lines[i]);
+        }
     }
 
     /**
@@ -64,8 +97,16 @@ public class StringCompress {
      * @return Anzahl der Zeilen in der Textdatei
      */
     public int getNoOfLines(String fileName) {
+        int count = 0;
+        try (Scanner scanner = new Scanner(new FileReader(fileName))) {
+            for (int i = 0; scanner.hasNextLine(); i++) {
+                scanner.nextLine();
+                count = i;
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
 
-
-        return -1;
+        return count;
     }
 }
